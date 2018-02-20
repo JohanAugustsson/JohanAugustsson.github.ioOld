719ccbfb1d88ca5b0fe8cf29344b925abdf6e18f
Johan Augustsson <j.augustsson80@gmail.com>
2018-02-20 08:59:21 +0100

    utv btn added

--- utveckling/test.js
+++ utveckling/test.js
@@ -8,8 +8,9 @@ let myTimer=()=>{
   console.log(i);
   snd.volume= 0.2;
   snd.play();
-}
+  //navigator.geolocation.getCurrentPosition(showPosition);
 
+}
 
 
 
@@ -18,11 +19,19 @@ let snd = new Audio("../Audio/button-1.wav"); // buffers automatically when crea
 window.addEventListener("load",function(event){
 
   let btnPlaySound = document.getElementById('btn-playSound');
-
   btnPlaySound.addEventListener('click',function(event){
     console.log("test");
     let myWatch= setInterval(myTimer,1000);
   })
 
+  navigator.geolocation.watchPosition(showPosition);
 
 })
+
+
+let showPosition =(position)=>{
+  let a = position
+  let positionContainer = document.getElementsByClassName('position')[0];
+  positionContainer.innerHTML += "<p>"+a.coords.latitude +"<br/>"+ a.coords.longitude+"</p>"
+  console.log(position);
+}
