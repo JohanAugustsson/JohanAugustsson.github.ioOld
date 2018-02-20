6aac5880ce506f1acd615a0342d640e6ab1600a1
Johan Augustsson <j.augustsson80@gmail.com>
2018-02-20 08:16:13 +0100

    utveckling added sound

--- utveckling/test.js
+++ utveckling/test.js
@@ -6,7 +6,11 @@ let i = 0;
 let myTimer=()=>{
   i++
   console.log(i);
+  snd.volume= 0.2;
+  snd.play();
 }
 
 
 let myWatch= setInterval(myTimer,1000);
+
+let snd = new Audio("../Audio/button-1.wav"); // buffers automatically when created
