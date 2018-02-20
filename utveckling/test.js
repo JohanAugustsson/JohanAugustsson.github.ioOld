a5a1ab6261e3e4f5bd310c012da454a52c211395
Johan Augustsson <j.augustsson80@gmail.com>
2018-02-20 10:49:08 +0100

    update

--- utveckling/test.js
+++ utveckling/test.js
@@ -12,19 +12,14 @@ let myTimer=()=>{
 }
 
 let snapTime = "";
-
+let startTime = 0;
 let showPosition =(position)=>{
   if(snapTime==position.timestamp){
-
   }else{
     snapTime=position.timestamp;
-
-  console.log(position);
-
-
   let positionContainer = document.getElementsByClassName('position')[0];
   //positionContainer.innerHTML += "<p>"+ now +"<br/>"+position.coords.latitude +"<br/>"+ position.coords.longitude+"</p>"
-  positionContainer.innerHTML += "<p>"+"time: "+position.timestamp+ "<br/>"+"latitude: "+position.coords.latitude +"<br/>"+"longitude: "+ position.coords.longitude+"</p>"
+  positionContainer.innerHTML += "<p>"+"time: "+ ((position.timestamp - startTime)/1000) + "<br/>"+"latitude: "+position.coords.latitude +"<br/>"+"longitude: "+ position.coords.longitude+"</p>"
   }
 }
 
@@ -32,20 +27,15 @@ let showPosition =(position)=>{
 let snd = new Audio("../Audio/button-1.wav");
 
 window.addEventListener("load",function(event){
-
   let btnPlaySound = document.getElementById('btn-playSound');
+  startTime = Date.now();
   //navigator.geolocation.watchPosition(showPosition);
   btnPlaySound.addEventListener('click',function(event){
-
-    let myWatch= setInterval(myTimer,1000);
+    let myWatch= setInterval(myTimer,2000);
   })
-
-
-
 })
 
 
 navigator.permissions.query({name:'geolocation'}).then(function(result) {
   console.log(result);
-
 });
