a2b9385890d31baae92b08652d2f7179fde9382f
Johan Augustsson <j.augustsson80@gmail.com>
2018-02-20 08:34:19 +0100

    utv btn added

--- utveckling/test.js
+++ utveckling/test.js
@@ -11,6 +11,18 @@ let myTimer=()=>{
 }
 
 
-let myWatch= setInterval(myTimer,1000);
+
 
 let snd = new Audio("../Audio/button-1.wav"); // buffers automatically when created
+
+window.addEventListener("load",function(event){
+
+  let btnPlaySound = document.getElementById('btn-playSound');
+
+  btnPlaySound.addEventListener('click',function(event){
+    console.log("test");
+    let myWatch= setInterval(myTimer,1000);
+  })
+
+
+})
