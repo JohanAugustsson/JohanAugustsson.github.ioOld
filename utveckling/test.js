ee2c2867bb3dbfc4fd7e3e288b30499779836661
Johan Augustsson <j.augustsson80@gmail.com>
2018-02-20 12:02:51 +0100

    update

--- utveckling/test.js
+++ utveckling/test.js
@@ -1,7 +1,7 @@
 
 let snd = new Audio("../Audio/button-1.wav");
 
-
+//lock.unlock();//
 
 let i = 0;
 let myTimer=()=>{
@@ -9,25 +9,26 @@ let myTimer=()=>{
   console.log(i);
   snd.volume= 0.5;
   snd.play();
-  //navigator.geolocation.getCurrentPosition(showPosition);
-  navigator.geolocation.watchPosition(showPosition);
+  navigator.geolocation.getCurrentPosition(showPosition);
+  //navigator.geolocation.watchPosition(showPosition);
 }
 
 let snapTime = "";
 let startTime = 0;
 let showPosition =(position)=>{
-  if(snapTime==position.timestamp){
-  }else{
+  //if(snapTime==position.timestamp){
+  //}else{
     snapTime=position.timestamp;
-  let positionContainer = document.getElementsByClassName('position')[0];
     let time = (position.timestamp - startTime)/1000
+    let positionContainer = document.getElementsByClassName('position')[0];
     positionContainer.innerHTML += "<p>"+"time: "+ time + "<br/>"+"latitude: "+position.coords.latitude +"<br/>"+"longitude: "+ position.coords.longitude+"</p>"
-  }
+  //}
 }
 
 
 
 window.addEventListener("load",function(event){
+
   let btnPlaySound = document.getElementById('btn-playSound');
   //navigator.geolocation.watchPosition(showPosition);
   btnPlaySound.addEventListener('click',function(event){
