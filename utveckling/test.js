82a638018bc61c66e0754df9943672b75dd16c8b
Johan Augustsson <j.augustsson80@gmail.com>
2018-02-20 10:54:57 +0100

    update

--- utveckling/test.js
+++ utveckling/test.js
@@ -1,4 +1,5 @@
 
+let snd = new Audio("../Audio/button-1.wav");
 
 
 
@@ -18,19 +19,18 @@ let showPosition =(position)=>{
   }else{
     snapTime=position.timestamp;
   let positionContainer = document.getElementsByClassName('position')[0];
-  //positionContainer.innerHTML += "<p>"+ now +"<br/>"+position.coords.latitude +"<br/>"+ position.coords.longitude+"</p>"
-  positionContainer.innerHTML += "<p>"+"time: "+ ((position.timestamp - startTime)/1000) + "<br/>"+"latitude: "+position.coords.latitude +"<br/>"+"longitude: "+ position.coords.longitude+"</p>"
+    let time = (position.timestamp - startTime)/1000
+    positionContainer.innerHTML += "<p>"+"time: "+ time + "<br/>"+"latitude: "+position.coords.latitude +"<br/>"+"longitude: "+ position.coords.longitude+"</p>"
   }
 }
 
 
-let snd = new Audio("../Audio/button-1.wav");
 
 window.addEventListener("load",function(event){
   let btnPlaySound = document.getElementById('btn-playSound');
-  startTime = Date.now();
   //navigator.geolocation.watchPosition(showPosition);
   btnPlaySound.addEventListener('click',function(event){
+    startTime = Date.now();
     let myWatch= setInterval(myTimer,2000);
   })
 })
